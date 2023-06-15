d32b698206ef2630622df2f64b842d86d152616b
RabiieOuallaf <rabie.ouallaf400@hotmail.com>
2023-06-15 16:28:27 +0100

    Make transaction and get transaction list

--- transactions-front/src/app/transcations/transcations.component.ts
+++ transactions-front/src/app/transcations/transcations.component.ts
@@ -1,5 +1,7 @@
+import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-transcations',
@@ -8,57 +10,91 @@ import { AuthService } from '../auth.service';
 })
 export class TranscationsComponent {
 
-  displayAccountList : boolean = false;
-  displayTransactionMenu : boolean = false;
-  showRib : boolean = false;
-  sold : number = 0;
+  displayAccountList: boolean = false;
+  displayTransactionMenu: boolean = false;
+  sold: number = 0;
+  userAccounts: any[] = [];
+  userTransactions : any[] = [];
 
-  constructor(public auth: AuthService) {
+  constructor(public auth: AuthService, public accountsAndTransactions: DataService) {
+
+  }
+
+  ngOnInit(): void {
+    this.displayUserAccounts();
+    this.displayUserTransactions();
 
   }
 
   // ==== Toggle methods ==== //
 
-   toggleAccountList() {
+  toggleAccountList() {
     this.displayAccountList = !this.displayAccountList;
-   }
-   toggleTransactionMenu() {
+  }
+  toggleTransactionMenu() {
     this.displayTransactionMenu = !this.displayTransactionMenu;
-   }
-   toggleRibVisiblity(selectedValue : string) {
-    this.showRib = selectedValue !== 'IN';
-    console.log('this.showRib');
-   }
+  }
+  
 
-   
 
-   // ==== Transaction methods ==== //
-   transactedAmount : number = 0;
-   operationType : string = 'IN';
 
-   setTransactedAmount(value : number) {
+  // ==== Transaction methods ==== //
+  transactedAmount: number = 0;
+  operationType: string = 'in';
+  operationTitle : string = '';
+
+  setTransactedAmount(value: number) {
     this.transactedAmount = value;
-   }
+  }
 
-   setOperationMode(value : string) {
+  setOperationMode(value: string) {
     this.operationType = value;
-   }
+  }
 
-   makeTransaction() {
-     
-     if(this.operationType == 'OUT'){
-        if(this.transactedAmount <= this.sold){
+  setOperationTitle(value: string) {
+    this.operationTitle = value;
+  }
+
+
+
+  makeTransaction() {
+
+    if (this.operationType == 'out') {
+      if (this.transactedAmount <= this.sold) {
 
         this.sold = this.sold - this.transactedAmount;
+        this.accountsAndTransactions.createTransaction(this.transactedAmount,2,this.operationTitle,this.operationType); 
 
-      }else {
+
+      } else {
         alert('Something went wrong');
       }
     }
 
-    if(this.operationType == 'IN') {
+    if (this.operationType == 'in') {
       this.sold = this.sold + this.transactedAmount;
+        this.accountsAndTransactions.createTransaction(this.transactedAmount,2,this.operationTitle,this.operationType); 
     }
-   }
+  }
+
+  // displaying and switching accounts 
+
+  displayUserAccounts() {
+    const userString = localStorage.getItem("user");
+    const user = userString ? JSON.parse(userString) : null;
+    const userId = user?.uid;
+
+    this.accountsAndTransactions.getUserAccounts(userId)
+      .subscribe(accounts => {
+        this.userAccounts = accounts;
+      });
+  }
+  displayUserTransactions() {
+    this.accountsAndTransactions.getUserTransactions(12345678)
+        .subscribe(transactions => {
+          this.userTransactions = transactions;
+        })
+  }
+
 
 }
