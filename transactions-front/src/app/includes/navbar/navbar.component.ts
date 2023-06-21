5be1ffb8cd741b5a28b0b8a1abfb9c09d8809e22
AnasNAK <anasc00ding@gmail.com>
2023-06-21 16:21:06 +0100

    features

--- transactions-front/src/app/includes/navbar/navbar.component.ts
+++ transactions-front/src/app/includes/navbar/navbar.component.ts
@@ -2,32 +2,27 @@ import { Component } from '@angular/core';
 import { AuthService } from 'src/app/auth.service';
 
 @Component({
-    selector: 'app-navbar',
-    templateUrl: './navbar.component.html',
-    styleUrls: ['./navbar.component.css'],
-
-
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
 })
-
-
 export class NavbarComponent {
+  constructor(public auth: AuthService) {}
 
+  navigationOptions = [
+    { label: 'Transactions', icon: '../../../assets/icons/card-payment.png' },
+    { label: 'Users', icon: '../../../assets/icons/team.png' },
+  ];
 
-    constructor(public auth: AuthService) {
-
+  getUserEmail() {
+    const user = JSON.parse(localStorage.getItem('user')!);
+    if (user && user.email) {
+      return user.email;
     }
+    return '';
+  }
 
-    navigationOptions = [
-         { label: 'Transactions', icon: '../../../assets/icons/card-payment.png' },
-        { label: 'Users', icon: '../../../assets/icons/team.png' },
-    ];
-
-
-    getUserEmail() {
-        const user = JSON.parse(localStorage.getItem('user')!);
-        if (user && user.email) {
-          return user.email;
-        }
-        return ''; 
-      }
+  signOut() {
+    this.auth.SignOut(); 
+  }
 }
