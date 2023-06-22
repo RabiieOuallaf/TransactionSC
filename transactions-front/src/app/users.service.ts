15fff6e5266dcb093b3de3e9d6da4e0550b1f6d5
AnasNAK <anasc00ding@gmail.com>
2023-06-22 20:25:07 +0100

    add logout

--- transactions-front/src/app/users.service.ts
+++ transactions-front/src/app/users.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { AuthUser } from './../models/interfaces.type';
+import { Account, AuthUser, user } from './../models/interfaces.type';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -24,4 +26,49 @@ export class UsersService {
 
     return this.usersCollection.doc(uid).set(userWithId);
   }
+
+  updateUser(user: AuthUser): Promise<void> {
+    const { uid, ...userwithoutId } = user
+    return this.usersCollection.doc(uid).update(userwithoutId)
+  }
+  deleteUser(uid: string): Promise<void> {
+    return this.usersCollection.doc(uid).delete();
+
+  }
+
+  createAccount(account: Account, userId: string): Promise<boolean> {
+    const accountId = this.firestore.createId();
+    const accountWithId: Account = { ...account, id: accountId };
+  
+    const accountsCollectionRef = this.usersCollection
+      .doc(userId)
+      .collection('accounts');
+  
+    // Check the current number of documents in the subcollection
+    return accountsCollectionRef.ref.get()
+      .then((snapshot) => {
+        if (snapshot.size >= 5) {
+          throw new Error('Maximum number of accounts reached.');
+        } else {
+          // Create the new account document
+          return accountsCollectionRef.doc(accountId).set(accountWithId);
+        }
+      })
+      .then(() => {
+        // Account created successfully
+        return true; // Return true indicating successful creation
+      })
+      .catch((error) => {
+        // Handle the error
+        console.error('Error creating account:', error);
+        return false; // Return false indicating failure
+      });
+  }
+  
+  
+  
+  
+  
+  
+  
 }
